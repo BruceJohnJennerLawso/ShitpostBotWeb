6813566867b7602605104ef1889eeaa14ec91799
ShitpostBot <brett.orange.memes@gmail.com>
2016-02-25 10:28:32 +1100

    fixed pagination on pending galleries

---
+++ spb/src-gallery-pending.php
@@ -0,0 +1,54 @@
+<?php
+$ITEMS_PER_PAGE = 30;
+include('php/autoload.php');
+require("php/gallery-item.php");
+$by = isset($_GET['by']) ? $_GET['by'] : null;
+$dir = isset($_GET['dir']) ? $_GET['dir'] : null;
+$p = isset($_GET['p']) ? $_GET['p'] : null;
+$_SESSION['lastSrcGalleryPage'] = "src-gallery-pending.php?by=$by&dir=$dir&p=$p";
+
+$orderByReq = !is_null($by) ? $by : 'time-acc';
+switch($orderByReq){
+	case 'submitter':
+		$orderBy = 'ORDER BY u.username';
+		break;
+		
+	case 'time-up':
+		$orderBy = 'ORDER BY s.timeAdded';
+		break;
+		
+	case 'time-acc':
+		$orderBy = 'ORDER BY s.timeReviewed';
+		break;
+		
+	default:
+		$orderBy = "ORDER BY SUM(CASE WHEN r.isPositive = 'y' THEN 1 ELSE 0 END) - SUM(CASE WHEN r.isPositive = 'n' THEN 1 ELSE 0 END)";
+}
+
+$dir = !is_null($dir) ? ($dir == 'asc' ? 'asc' : 'desc') : 'desc';
+
+$totalItemCount = $db->scalar("SELECT count(sourceId) FROM SourceImages WHERE reviewState = 'p'", array(), array());
+$page = max(!is_null($p) ? $p : 1, 1);
+
+$startIndex = ($page-1) * $ITEMS_PER_PAGE;
+$query = "SELECT s.* FROM Users as u, SourceImages as s LEFT OUTER JOIN SourceRatings as r ON  r.sourceId = s.sourceId WHERE s.userId = u.userId AND reviewState = 'p' GROUP BY s.sourceId  $orderBy $dir LIMIT ? OFFSET ?";
+$sourceImages = $db->getSourceImages($query, array($ITEMS_PER_PAGE, $startIndex), array(SQLITE3_INTEGER, SQLITE3_INTEGER));
+$items = array();
+foreach($sourceImages as $sourceImage){
+	$query = 'SELECT isPositive FROM SourceRatings WHERE userId = ? AND sourceId = ?';
+	$userRating = $isLoggedIn ? $db->scalar($query, array($me->getUserId(), $sourceImage->getSourceId()), array_fill(0, 2, SQLITE3_TEXT)) : false;
+	array_push($items, new GalleryItem(
+		$sourceImage->getSourceId(), 
+		$sourceImage->getImage(), 
+		"view-srcimg.php?id=".$sourceImage->getSourceId(), 
+		$sourceImage->getRating(), 
+		new User($sourceImage->getUserId(), $sourceImage->getSubmitterName()),
+		$userRating === false ? '-' : ($userRating === 'y' ? 'p' : 'n')
+	));
+}
+
+$pageCount = max(ceil($totalItemCount / $ITEMS_PER_PAGE), 1);
+$page = min($page, $pageCount);
+echo $twig->render('gallery.html', array('items' => $items, 'type' => 's', 'currentPage' => $page, 'pageCount' => $pageCount, 'orderBy' => $orderByReq, 'dir' => $dir));
+$db->close();
+?>
---
+++ spb/t6e-gallery-pending.php
@@ -0,0 +1,54 @@
+<?php
+$ITEMS_PER_PAGE = 30;
+include('php/autoload.php');
+require("php/gallery-item.php");
+$by = isset($_GET['by']) ? $_GET['by'] : null;
+$dir = isset($_GET['dir']) ? $_GET['dir'] : null;
+$p = isset($_GET['p']) ? $_GET['p'] : null;
+$_SESSION['lastT6eGalleryPage'] = "t6e-gallery-pending.php?by=$by&dir=$dir&p=$p";
+
+$orderByReq = !is_null($by) ? $by : 'time-acc';
+switch($orderByReq){
+	case 'submitter':
+		$orderBy = 'ORDER BY u.username';
+		break;
+		
+	case 'time-up':
+		$orderBy = 'ORDER BY t.timeAdded';
+		break;
+		
+	case 'time-acc':
+		$orderBy = 'ORDER BY t.timeReviewed';
+		break;
+		
+	default:
+		$orderBy = "ORDER BY SUM(CASE WHEN r.isPositive = 'y' THEN 1 ELSE 0 END) - SUM(CASE WHEN r.isPositive = 'n' THEN 1 ELSE 0 END)";
+}
+
+$dir = !is_null($dir) ? ($dir == 'asc' ? 'asc' : 'desc') : 'desc';
+
+$totalItemCount = $db->scalar("SELECT count(templateId) FROM Templates WHERE reviewState = 'p'", array(), array());
+$page = max(!is_null($p) ? $p : 1, 1);
+
+$startIndex = ($page-1) * $ITEMS_PER_PAGE;
+$query = "SELECT t.* FROM Users as u, Templates as t LEFT OUTER JOIN TemplateRatings as r ON r.templateId = t.templateId WHERE t.userId = u.userId AND reviewState = 'p' GROUP BY t.templateId $orderBy $dir LIMIT ? OFFSET ?";
+$templates = $db->getTemplates($query, array($ITEMS_PER_PAGE, $startIndex), array(SQLITE3_INTEGER, SQLITE3_INTEGER));
+$items = array();
+foreach($templates as $template){
+	$query = 'SELECT isPositive FROM TemplateRatings WHERE userId = ? AND templateId = ?';
+	$userRating = $isLoggedIn ? $db->scalar($query, array($me->getUserId(), $template->getTemplateId()), array_fill(0, 2, SQLITE3_TEXT)) : false;
+	array_push($items, new GalleryItem(
+		$template->getTemplateId(), 
+		$template->getImage(), 
+		"view-t6e.php?id=".$template->getTemplateId(), 
+		$template->getRating(), 
+		new User($template->getUserId(), $template->getSubmitterName()),
+		$userRating === false ? '-' : ($userRating === 'y' ? 'p' : 'n')
+	));
+}
+
+$pageCount = max(ceil($totalItemCount / $ITEMS_PER_PAGE), 1);
+$page = min($page, $pageCount);
+echo $twig->render('gallery.html', array('items' => $items, 'type' => 't', 'currentPage' => $page, 'pageCount' => $pageCount, 'orderBy' => $orderByReq, 'dir' => $dir));
+$db->close();
+?>
